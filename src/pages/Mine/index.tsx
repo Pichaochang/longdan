1d9ee61dafa2f3e9a059c0832db3a9a65086a20b
pichao chang <Pichaochang@163.com>
2024-03-13 10:28:22 +0800

    feat、1

--- src/pages/Mine/index.tsx
+++ src/pages/Mine/index.tsx
@@ -11,7 +11,13 @@ const Mine = () => {
   const [inputValue, setInputValue] = useState(undefined as any);
   const [loading, setLoading] = useState(true);
   const [address, setAddress] = useState('');
-
+  const formatAddress:any = function(a:any) {
+    const firstSix = a.substr(0, 6);
+    const lastSix = a.substr(-6);
+    console.log("前6位:", firstSix);
+    console.log("后6位:", lastSix);
+    return `${firstSix}...${lastSix}`
+  }
 
 
   const changeLoading = (load: boolean) => {
@@ -78,18 +84,18 @@ const Mine = () => {
         <img className="header-left-img" src="/BTB.png" alt="" />
         <div className="header-right">
           <img className="header-right-img" src="/1.png" alt="" />
-          <div>{address}</div>
+          <div>{formatAddress(address)}</div>
         </div>
       </div>
       <div className="egg-div">
         <img src="/egg.png" alt="" />
       </div>
-      <div className="text-bit">
+      <div className="text-bit" style={{marginTop: '-30px', position: 'relative', fontSize: '16px'}}>
         Bit world（比特世界）Wbe.3生態。 Bit world由美國矩陣資本領投聯合澳大利亞加密貨幣研發組織發起； 基於幣安智令78% C完全去中心化Wbe.3金融商業生態，打造Wbe.3社交軟件與元宇宙平臺：為引流龐大用戶參與，平臺發行代幣BTB作為未來整個生態應用通證助力參與者實現自身價值。
       </div>
 
       <div className="text-bit">
-        <div className="long-text">1龙蛋+1USDT兑换=1BTB</div>
+        <div className="long-text">{inputValue}龍蛋+{inputValue}USDT兌換={inputValue}BTB</div>
         <div className={styles.content}>
           <InputCard onChange={onChange} inputValue={inputValue} />
           <Button
@@ -101,9 +107,10 @@ const Mine = () => {
               color: "#fff",
               width: "100%",
               height: "40px",
+              marginTop: '40px'
             }}
           >
-            确认兑换
+            確認兌換
           </Button>
           <div className="web3-text">web3|元宇宙</div>
         </div>
